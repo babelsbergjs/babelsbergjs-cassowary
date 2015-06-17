6941d01ff40033f947940113aafb46e8fb12c039
FHorschig <f.horschig@googlemail.com>
2015-06-17 17:26:19 +0200

    Self-testing index.js and working on npm install. Increased version.

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ bbb.always({solver: s, ctx: {obj: obj}}, function () {
   return obj.a + 7 <= obj.b;
 });
 obj.a = 10;
+
+console.log("Testing ClSimplexSolver solves 'always'-condition...")
 if(!(obj.a + 7 <= obj.b))
   throw("AssertionError: Expecting obj.b (" + obj.b + ") to be at least 7 + obj.a (" + obj.a + ")")
+else
+  console.log('... successfull!')
